5045a2524aad185e6673130cf623342ba42d7d04
chris <chris.clarke@talis.com>
2014-05-28 20:49:58 +0100

    Attempting to integrate send to babel

--- post-commit.py
+++ post-commit.py
@@ -21,7 +21,7 @@ from datetime import datetime
 GITSHOTS_PATH = os.getenv('GITSHOTS_PATH', '~/.gitshots/')
 GITSHOTS_SERVER_URL = os.getenv(
     'GITSHOTS_SERVER_URL',
-    'http://gitshots.ranman.org')
+    'http://talis-gitshots.herokuapp.com/')
 GITSHOTS_IMAGE_CMD = os.getenv(
     'GITSHOTS_IMG_CMD',
     'imagesnap -q ')
